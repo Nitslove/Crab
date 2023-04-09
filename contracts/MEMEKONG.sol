9587deb375e91b9847f48b3b1d23326321bb4ad2
nitslove <beejawatnitesh@gmail.com>
2023-04-10 02:56:30 +0530

    TestCases and Contract Modification

--- contracts/MEMEKONG.sol
+++ contracts/MEMEKONG.sol
@@ -295,8 +295,7 @@ contract MEMEKONG is IERC20, TokenEvents {
     uint8 public constant decimals = 9;
 
     //admin
-    address constant internal _P1 = 0x4556a3bF9633aAcB73039004364A73a437606dE1;
-    address constant internal _P2 = 0x8CBCA3836E2e99DB797793E42328D48eEA665D4b;
+    address private _P1;
     bool public isLocked = false;
     bool private sync;
     
@@ -325,153 +324,51 @@ contract MEMEKONG is IERC20, TokenEvents {
     }
 
     constructor(uint256 initialTokens) {
+        _P1 = msg.sender;
         admins[_P1] = true;
         admins[msg.sender] = true;
         //mint initial tokens
         mintInitialTokens(initialTokens);
     }
-
     
-    /**
-     * @dev See {IERC20-totalSupply}.
-     */
     function totalSupply() external view override returns (uint256) {
         return _totalSupply;
     }
-
-    /**
-     * @dev See {IERC20-balanceOf}.
-     */
+    
     function balanceOf(address account) public view override returns (uint256) {
         return _balances[account];
     }
-
-    /**
-     * @dev See {IERC20-transfer}.
-     *
-     * Requirements:
-     *
-     * - `recipient` cannot be the zero address.
-     * - the caller must have a balance of at least `amount`.
-     */
+    
     function transfer(address recipient, uint256 amount) external override returns (bool) {
         _transfer(msg.sender, recipient, amount);
         return true;
     }
-
-    /**
-     * @dev See {IERC20-allowance}.
-     */
+    
     function allowance(address owner, address spender) external view override returns (uint256) {
         return _allowances[owner][spender];
     }
-
-    /**
-     * @dev See {IERC20-approve}.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
+    
     function approve(address spender, uint256 amount) external override returns (bool) {
         _approve(msg.sender, spender, amount);
         return true;
     }
-
-    /**
-     * @dev See {IERC20-transferFrom}.
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20};
-     *
-     * Requirements:
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for `sender`'s tokens of at least
-     * `amount`.
-     */
      
     function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
         _transfer(sender, recipient, amount);
         _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount, "ERC20: transfer amount exceeds allowance"));
         return true;
     }
-
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
+    
     function increaseAllowance(address spender, uint256 addedValue) external returns (bool) {
         _approve(msg.sender, spender, _allowances[msg.sender][spender].add(addedValue));
         return true;
     }
-
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
+    
     function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
         _approve(msg.sender, spender, _allowances[msg.sender][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
         return true;
     }
-
-    /**
-     * @dev Moves tokens `amount` from `sender` to `recipient`
-     *
-     * This is internal function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * Requirements:
-     *
-     * - `sender` cannot be the zero address.
-     * - `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     */
-    // Nitish - Commission on transfer.
-    // 2% burned
-    // 7% to ADMIN _P1
-    function _transfer(address sender, address recipient, uint256 amount) internal {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
-        uint256 burnAmount = amount.mul(2).div(100);
-        uint256 adminCommissionAmount = amount.mul(7).div(100);
-        uint256 recipientAmount = amount.sub(burnAmount).sub(adminCommissionAmount);
-        BurnMkong(burnAmount);
-        _balances[_P1] = _balances[_P1].add(recipientAmount);
-        _balances[recipient] = _balances[recipient].add(recipientAmount);
-        emit Transfer(sender, recipient, amount);
-    }
-
-    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-     * the total supply unless mintBLock is true
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * Requirements
-     *
-     * - `to` cannot be the zero address.
-     */
+    
     function _mint(address account, uint256 amount) internal {
         uint256 amt = amount;
         require(account != address(0), "ERC20: mint to the zero address");
@@ -479,69 +376,36 @@ contract MEMEKONG is IERC20, TokenEvents {
         _balances[account] = _balances[account].add(amt);
         emit Transfer(address(0), account, amt);
     }
-
-    /**
-     * @dev Destroys `amount` tokens from `account`, reducing the
-     * total supply.
-     *
-     * Emits a {Transfer} event with `to` set to the zero address.
-     *
-     * Requirements
-     *
-     * - `account` cannot be the zero address.
-     * - `account` must have at least `amount` tokens.
-     */
+    
     function _burn(address account, uint256 amount) internal {
         require(account != address(0), "ERC20: burn from the zero address");
         _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
         _totalSupply = _totalSupply.sub(amount);
         emit Transfer(account, address(0), amount);
     }
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
-     *
-     * This is internal function is equivalent to `approve`, and can be used to
-     * e.g. set automatic allowances for certain subsystems, etc.
-     *
-     * Emits an {Approval} event.
-     *
-     * Requirements:
-     *
-     * - `owner` cannot be the zero address.
-     * - `spender` cannot be the zero address.
-     */
+    
     function _approve(address owner, address spender, uint256 amount) internal {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
-
-    /**
-     * @dev Destroys `amount` tokens from `account`.`amount` is then deducted
-     * from the caller's allowance.
-     *
-     * See {_burn} and {_approve}.
-     */
+    
     function _burnFrom(address account, uint256 amount) internal {
         _burn(account, amount);
         _approve(account, msg.sender, _allowances[account][msg.sender].sub(amount, "ERC20: burn amount exceeds allowance"));
     }
-
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    // event Transfer(address indexed from, address indexed to, uint256 value);//from address(0) for minting
-
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
-    // event Approval(address indexed owner, address indexed spender, uint256 value);
+    
+    // Nitish - Commission on transfer.
+    // 2% burned
+    // 7% to ADMIN _P1
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
 
     //mint memekong initial tokens (only ever called in constructor)
     function mintInitialTokens(uint amount)
@@ -630,7 +494,6 @@ contract MEMEKONG is IERC20, TokenEvents {
             //reset staking timestamp
             staker[msg.sender].stakeStartTimestamp = block.timestamp;
             _mint(_P1, interest.mul(7).div(100));//7% admin P1 copy
-            // _mint(_P2, interest.mul(2).div(100));//2% admin P2 copy
         }
     }
     
@@ -647,7 +510,6 @@ contract MEMEKONG is IERC20, TokenEvents {
             _mint(msg.sender, interest);
             staker[msg.sender].totalStakingInterest += interest;
             _mint(_P1, interest.mul(7).div(100));//7% admin P1 copy
-            // _mint(_P2, interest.mul(2).div(100));//2% admin P2 copy
         }
     }
 
@@ -714,7 +576,9 @@ contract MEMEKONG is IERC20, TokenEvents {
                 }
             }
         }
-        return (staked.mul(minsPastStakeTime(_user)).div(apyAdjust).div(1314));
+        
+        return (staked.mul(minsPastStakeTime(_user)).div(apyAdjust).div(1251));
+        // return (staked.mul(minsPastStakeTime(_user)).div(apyAdjust).div(1314));
     }
 
     //returns amount of minutes past since stake start
